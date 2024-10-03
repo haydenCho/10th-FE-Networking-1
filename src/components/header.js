674119e5ffc36f61809516f166661ace6db266ac
EunsolnCho <cho_eunsol@naver.com>
2024-10-03 17:54:23 +0900

    헤더 상수화 피드백 반영

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,17 @@
 import { getDate } from "../utils/getDate.js";
+import { HEADER_CONSTANTS } from "../utils/constants.js";
 
 export function renderHeader() {
   const imageWrap = document.getElementById("header-image-wrap");
   const dateWrap = document.getElementById("header-date-wrap");
 
   const headerImage = document.createElement("img");
-  headerImage.src = `/src/assets/images/title.png`;
-  headerImage.alt = "title";
+  headerImage.src = HEADER_CONSTANTS.IMAGE_SRC;
+  headerImage.alt = HEADER_CONSTANTS.IMAGE_ALT;
   imageWrap.appendChild(headerImage);
 
   const headerTitle = document.createElement("p");
-  headerTitle.textContent = `뉴스스탠드`;
+  headerTitle.textContent = HEADER_CONSTANTS.TITLE;
   imageWrap.appendChild(headerTitle);
 
   const headerDate = document.createElement("p");
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const HEADER_CONSTANTS = {
+  IMAGE_SRC: "/src/assets/images/title.png",
+  IMAGE_ALT: "title",
+  TITLE: "뉴스스탠드",
+};
