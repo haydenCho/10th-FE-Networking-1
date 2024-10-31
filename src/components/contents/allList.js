7f21ec50c09bbf4a9722211c2375a936cedfd43f
EunsolnCho <cho_eunsol@naver.com>
2024-10-31 19:24:53 +0900

    뉴스 리스트 보기 초기 UI 구현

--- src/components/contents/allList.js
+++ src/components/contents/allList.js
@@ -1,4 +1,23 @@
 export default function renderAllList() {
   const typeTabArea = document.querySelector('.news-content-area');
-  typeTabArea.innerHTML = `<h3>전체 언론사 리스트뷰</h3>`;
+  typeTabArea.innerHTML = `
+    <section class="news-category-area">카테고리바</section>
+    <section class="news-content-container">
+      <section class="news-content-header">
+        <div class="news-content-logo">신문사 로고</div>
+        <div class="news-content-editAt">편집한 날짜</div>
+        <div class="news-content-subscribeBtn">구독하기 버튼</div>
+      </section>
+      <section class="news-content">
+        <section class="left-content">
+          <div class="main-news-image"></div>
+          <div class="main-news-title">메인 뉴스 타이틀</div>
+        </section>
+        <section class="right-content">
+          <div class="sub-news-title"></div>
+          <div class="edit-script">서울경제 언론사에서 직접 편집한 뉴스입니다.</div>
+        </section>
+      </section>
+    </section>
+  `;
 };
