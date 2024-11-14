e117c2c9f56746749049499fffa75906eb5a03cb
EunsolnCho <cho_eunsol@naver.com>
2024-11-14 17:17:37 +0900

    뉴스 컨텐츠 데이터 생성 및 적용

--- src/components/contents/allList.js
+++ src/components/contents/allList.js
@@ -1,23 +1,31 @@
 import { renderListCategory } from "../listCategory.js";
 
-export default function renderAllList() {
+export default async function renderAllList() {
+  // 데이터 불러오기(경로 주의)
+  const response = await fetch("src/data/newsAllListData.json")
+  const newsContentData = await response.json();
+
   const typeTabArea = document.querySelector('.news-content-area');
   typeTabArea.innerHTML = `
     <section class="news-category-area"></section>
     <section class="news-content-container">
       <section class="news-content-header">
-        <div class="news-content-logo">신문사 로고</div>
-        <div class="news-content-editAt">편집한 날짜</div>
+        <img src=${newsContentData.press_image_url} alt="${newsContentData.press} 로고">
+        <div class="news-content-editAt">${newsContentData.date}</div>
         <div class="news-content-subscribeBtn">구독하기 버튼</div>
       </section>
       <section class="news-content">
         <section class="left-content">
-          <div class="main-news-image"></div>
-          <div class="main-news-title">메인 뉴스 타이틀</div>
+          <div class="main-news-image">
+            <img src=${newsContentData.news_image_url} alt="메인 뉴스 이미지">
+          </div>
+          <div class="main-news-title">${newsContentData.title}</div>
         </section>
         <section class="right-content">
-          <div class="sub-news-title"></div>
-          <div class="edit-script">서울경제 언론사에서 직접 편집한 뉴스입니다.</div>
+          <div class="sub-news-title">
+            ${newsContentData.articles.map((article) => `<div class="sub-news-title-item">${article}</div>`).join('')}
+          </div>
+          <div class="edit-script">${newsContentData.press} 언론사에서 직접 편집한 뉴스입니다.</div>
         </section>
       </section>
     </section>
